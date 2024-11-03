8801c89f58757fcaa84e6091e07131abc4696674
gregor-uusvali <gregoruusvali@gmail.com>
2024-11-03 13:06:55 +0200

    added random number for trefle api feature

--- src/app/graphql/graphql.component.ts
+++ src/app/graphql/graphql.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graphql',
   templateUrl: './graphql.component.html',
   styleUrls: ['./graphql.component.css']
 })
-export class GraphqlComponent {
+export class GraphqlComponent implements OnInit {
+  constructor(private http: HttpClient) {
 
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost:8080/api/v1/dailyRandom").subscribe((data) => {
+      console.log(data)
+    })
+  }
 }
